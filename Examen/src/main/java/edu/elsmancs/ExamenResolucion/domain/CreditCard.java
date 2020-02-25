aae7124edd11f282e0a13070598ca474e02795ec
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 09:39:23 +0100

    feat(CreditCard): metodo pay

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
@@ -5,13 +5,15 @@ public class CreditCard {
     private String owner = "";
     private String number = "";
     private double credit = 3000d;
-    private final String SYMBOL = "EZIS";
+    private final String SYMBOL = "EZI";
 
 
     public CreditCard(String name, String number) {
         this.owner = owner;
         this.number = number;
     }
+
+    public CreditCard() {}
 
     public String getOwner() {
         return this.owner = owner;
@@ -23,6 +25,21 @@ public class CreditCard {
 
     public double credit() {
         return this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Owner = " + getOwner() + "\n" +
+                "Number = " + number() + "\n" +
+                "Credit = " + credit() + this.SYMBOL +
+                "\n";
+    }
+
+    public boolean pay(double pago) {
+        if (credit >= pago){
+            credit -= pago;
+        }
+        return credit >= pago;
     }
 
 }
