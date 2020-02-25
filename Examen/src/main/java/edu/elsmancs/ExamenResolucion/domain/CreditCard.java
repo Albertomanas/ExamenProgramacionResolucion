4eec0c90324f9b73687d5669f1daa0883327100b
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 09:01:03 +0100

    feat(CreditCard): Getters: getName, number y credit hechos
    Falta añadir ToString y pay

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
@@ -5,12 +5,24 @@ public class CreditCard {
     private String owner = "";
     private String number = "";
     private double credit = 3000d;
-    private String symbol = "EZIS";
+    private final String SYMBOL = "EZIS";
 
     private String name= "";
 
     public CreditCard(String name, String number) {
         this.name = name;
         this.number = number;
+    }
+
+    public String getName() {
+        return this.name = name;
+    }
+
+    public String number() {
+        return this.number = number;
+    }
+
+    public double credit() {
+        return this.credit = credit;
     }
 }
