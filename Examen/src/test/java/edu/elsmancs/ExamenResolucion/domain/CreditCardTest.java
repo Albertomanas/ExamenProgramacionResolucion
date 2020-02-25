d6ffbda86c3cba061ac196571431ab5507275309
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 08:59:04 +0100

    test(CreditCardTest): Caso crear tarjeta y verificar datos implementado

--- Examen/src/test/java/edu/elsmancs/ExamenResolucion/domain/CreditCardTest.java
+++ Examen/src/test/java/edu/elsmancs/ExamenResolucion/domain/CreditCardTest.java
@@ -1,4 +1,24 @@
 package edu.elsmancs.ExamenResolucion.domain;
 
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CreditCardTest {
+
+    @Test
+    public void comprobarDatosTarjetaTest() {
+        CreditCard creditCard = new CreditCard("Abradolf Lincler", "3452234634523457");
+        assertEquals(creditCard.getName(), "Abradolf Lincler");
+        assertNotNull(creditCard.number(), "3452234634523457");
+    }
+
+    @Test
+    public void crearTarjetaTest() {
+        CreditCard creditCard = new CreditCard("Abradolf Lincler","3452234634523457");
+        double delta = 0;
+        assertEquals(3000d, creditCard.credit(), delta);
+        assertNotNull(creditCard);
+    }
+
 }
