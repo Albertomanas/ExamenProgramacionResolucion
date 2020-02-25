1bc9e15e200d69d586209e0825df1da90e859dd1
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 12:00:24 +0100

    test(CristalExpenderTest): crear pack y dispatch testeado

--- Examen/src/test/java/edu/elsmancs/ExamenResolucion/domain/CristalExpenderTest.java
+++ Examen/src/test/java/edu/elsmancs/ExamenResolucion/domain/CristalExpenderTest.java
@@ -1,4 +1,27 @@
 package edu.elsmancs.ExamenResolucion.domain;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CristalExpenderTest {
+
+
+    @Test
+    public void crearPackTest() {
+        CristalExpender cristalExpender = new CristalExpender(3, 50d);
+        double delta = 0d;
+        assertNotNull(cristalExpender);
+        assertEquals(3, cristalExpender.stock(), delta);
+    }
+
+    @Test
+    public void dispatchPackTest() {
+        CristalExpender cristalExpender = new CristalExpender(3, 50d);
+        CreditCard creditCard = new CreditCard("Abradolf Lincler", "3452234634523457");
+        double delta = 0d;
+        cristalExpender.dispatch(creditCard);
+        assertEquals(2, cristalExpender.stock(), delta);
+        assertEquals(1950, creditCard.credit(), delta);
+    }
+
 }
