53b7b12bb34f9fc658b80c169b89f729453cba8f
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 12:03:38 +0100

    feat(CristalExpender): Try catch en dispatch completado
    Captación de excepciones y dispatch tratando esas excepciones

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CristalExpender.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CristalExpender.java
@@ -13,4 +13,23 @@ public class CristalExpender {
         return stock;
     }
 
+
+    //DISPATCH
+    private void require(Boolean holds) throws  Exception {
+        if (!holds) {
+            throw new Exception();
+        }
+    }
+   // BARRICADA comprobar que hay items en stock y que el saldo de la tarjeta de >= al coste del item
+    public void dispatch(CreditCard creditCard) {
+        try {
+            require(this.stock > 0);
+            require(creditCard.credit() >= itemCost);
+            /* Descontar de la tarjeta de crédito el coste del pack
+             * Restar 1 al stock cuando se realiza el pago
+             */
+            creditCard.pay(itemCost);
+            this.stock -= 1;
+        }catch (Exception e){}
+    }
 }
