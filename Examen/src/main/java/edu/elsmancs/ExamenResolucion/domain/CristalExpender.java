9ee6af937e2a70f85154e05e01294fe4326e93ba
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 11:37:20 +0100

    feat(CristalExpender): Metodo stock creado junto constructor

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CristalExpender.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CristalExpender.java
@@ -1,4 +1,16 @@
 package edu.elsmancs.ExamenResolucion.domain;
 
 public class CristalExpender {
+    private int stock = 0;
+    private double itemCost = 50d;
+
+    public CristalExpender(int stock, double itemCost) {
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
+
+    public int stock() {
+        return stock;
+    }
+
 }
