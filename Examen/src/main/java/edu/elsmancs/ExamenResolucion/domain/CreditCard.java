2d92f53de379304b4140ebf1520f81d999eea223
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-25 08:34:57 +0100

    feat(CreditCard): Constructuro y propiedades

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
@@ -1,4 +1,16 @@
 package edu.elsmancs.ExamenResolucion.domain;
 
 public class CreditCard {
+
+    private String owner = "";
+    private String number = "";
+    private double credit = 3000d;
+    private String symbol = "EZIS";
+
+    private String name= "";
+
+    public CreditCard(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
 }
