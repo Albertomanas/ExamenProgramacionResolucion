b49ed5640f076a1cbba2dbe737bd13970d3646d8
Albertomanas <amanas@cifpfbmoll.eu>
2020-05-17 15:44:30 +0200

    docs(CreditCard): Documentación JavaDoc

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/CreditCard.java
@@ -1,5 +1,10 @@
 package edu.elsmancs.ExamenResolucion.domain;
 
+/**
+ * Dicha clase representa la tarjeta de crédito de los invitados.
+ * @author Alberto Mañas
+ */
+
 public class CreditCard {
 
     private String owner = null;
@@ -7,6 +12,11 @@ public class CreditCard {
     private double credit = 3000d;
     private final String SYMBOL = "EZI";
 
+    /**
+     * Constructuror de la tarjeta de crédito.
+     * @param owner
+     * @param number
+     */
 
     public CreditCard(String owner, String number) {
         this.owner = owner;
@@ -15,18 +25,34 @@ public class CreditCard {
 
     public CreditCard() {}
 
+    /**
+     * Método que devuelve los owners.
+     * @return String owner
+     */
     public String owner() {
         return owner;
     }
 
+    /**
+     * Método que devuelve el número de tarjeta de crédito (público).
+     * @return String number
+     */
     public String number() {
         return number;
     }
 
+    /**
+     * Método que devuelve dicho crédito de la tarjeta.
+     * @return double credit
+     */
     public double credit() {
         return this.credit = credit;
     }
 
+    /**
+     * Método que convierte los getters del código en string, override a partir de la interfaz.
+     * @return String
+     */
     @Override
     public String toString() {
         return "\n" + "Owner = " + owner() + "\n" +
@@ -35,6 +61,11 @@ public class CreditCard {
                 "\n";
     }
 
+    /**
+     * Método que resta el pago al crédito de la tarjeta tras algún movimiento de esta.
+     * @param pago
+     * @return credit >= pago
+     */
     public boolean pay(double pago) {
         if (credit >= pago){
             credit -= pago;
