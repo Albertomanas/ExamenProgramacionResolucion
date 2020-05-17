e818603ec1b95cf17a607a9f9758472fa0177a47
Albertomanas <amanas@cifpfbmoll.eu>
2020-05-17 15:53:53 +0200

    docs(GuestDispatch): Documentación JavaDoc

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/GuestDispatcher.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/GuestDispatcher.java
@@ -1,6 +1,14 @@
 package edu.elsmancs.ExamenResolucion.domain;
 
+/**
+ * Interfaz para dicho programa
+ * @author Alberto Mañas
+ */
 public interface GuestDispatcher {
-
+    /**
+     * Interfaz que checkea la tarjeta de crédito de cada usuario para el uso de ella en los
+     * servicios de Risksy Business
+     * @param creditCard
+     */
     public void dispatch(CreditCard creditCard);
 }
