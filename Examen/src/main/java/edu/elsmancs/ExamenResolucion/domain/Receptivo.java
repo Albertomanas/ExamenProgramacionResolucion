97869b7963a2b23fd10c377b88cf99ea88b2ea77
Albertomanas <amanas@cifpfbmoll.eu>
2020-05-17 16:08:36 +0200

    docs(Receptivo): Documentación JavaDoc

--- Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/Receptivo.java
+++ Examen/src/main/java/edu/elsmancs/ExamenResolucion/domain/Receptivo.java
@@ -3,14 +3,26 @@ package edu.elsmancs.ExamenResolucion.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta clase automatiza los servicios que pueden adquirir los usuarios
+ * @author Alberto Mañas
+ */
 public class Receptivo {
 
     List<GuestDispatcher> dispatchers = new ArrayList<>();
 
+    /**
+     * Registra los items implementados por la interfaz.
+     * @param dispatch
+     */
     public void registra(GuestDispatcher dispatch) {
         this.dispatchers.add(dispatch);
     }
 
+    /**
+     * Método que recorre la estructura de dispatchers para todos los usuarios.
+     * @param CreditCard usuario
+     */
     public void dispatch(CreditCard usuario) {
         dispatchers.forEach( type -> type.dispatch(usuario)); //Expresión lambda (Parámetro) -> (Expresión)
     }
